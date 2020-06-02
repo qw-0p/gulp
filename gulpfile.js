14cb0a04c80759d2a07b75903498e3af476c260f
Aleksandr Sarkisov <quopiks@gmail.com>
2020-06-02 22:14:25 +0300

    added sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,9 @@
 const gult = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.taks('');
+gulp.taks('sass', () => {
+    return gulp
+        .src('app/scss/styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'));
+});
